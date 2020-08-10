f213559bedeb4343b6c0c617bd8efe8e03accede
ganesh-sadanala <ganesh8374387106@gmail.com>
2020-08-10 13:13:18 +0530

    Design the main page

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
 
+const api = {
+  key: process.env.REACT_APP_MY_API_KEY,
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 function App() {
   return (
     <div className="App">
-      <p>Hello</p>
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search... "
+          ></input>
+        </div>
+      </main>
     </div>
   );
 }
