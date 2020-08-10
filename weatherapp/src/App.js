d888fc45d1a0e9a48f462cd80497e526a88173c5
ganesh-sadanala <ganesh8374387106@gmail.com>
2020-08-10 14:29:39 +0530

    create a location box that shows location and date

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -5,6 +5,37 @@ const api = {
   base: "https://api.openweathermap.org/data/2.5/",
 };
 function App() {
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = d.getMonth();
+    let year = d.getFullYear();
+    return `${day} ${date} ${month} ${year}`;
+  };
   return (
     <div className="App">
       <main>
@@ -14,6 +45,10 @@ function App() {
             className="search-bar"
             placeholder="Search... "
           ></input>
+        </div>
+        <div className="Location-box">
+          <div className="Location">New York City US</div>
+          <div className="date">{dateBuilder(new Date())}</div>
         </div>
       </main>
     </div>
